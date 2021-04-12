20a5111955b97a3f09f124078b80f847734ea2bf
pinguimdeasadelta <64939397+pinguimdeasadelta@users.noreply.github.com>
2021-04-12 09:54:19 -0300

    Update montador.py

--- montador.py
+++ montador.py
@@ -1,14 +1,14 @@
 origem = input('Qual eh o nome do arquivo a ser convertido? (inclua a extensao)\n')
 nome = input('Digite o nome do programa resultante:')
-heximal = open(origem, "r")
+binary = open(origem, "r")
 desth = open(nome+'_H.drs', "w")
 destl = open(nome+'_L.drs', "w")
 desth.write('#A 0000h\n#B\n\n') #linha magica
 destl.write('#A 0000h\n#B\n\n') #linha magica
-for i in heximal:
+for i in binary:
     desth.write(i[:16] + '\n')
     destl.write(i[16:] + '\n')
 desth.close()
 destl.close()
-heximal.close()
+binary.close()
 print(f'Tudo pronto, os arquivos gerados foram: {nome}_L.drs e {nome}_H.drs')
